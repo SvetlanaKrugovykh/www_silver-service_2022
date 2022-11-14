591a5d829c6f0fb71c7c0aff30a91aa95f95e6db
SvetlanaKrugovykh <Svetlana.Krugovykh@gmail.com>
2022-11-14 06:57:17 +0200

    burger-menu added

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const nav = document.querySelector('.nav');
 const menuNav = document.querySelector('.menu-nav');
 const navItems = document.querySelectorAll('.menu-nav__item');
 const tHeader = document.querySelector("#t-header");
-
+const popup = document.querySelector("#popup-bm");
 
 let showMenu = false;
 choiceHeader();
@@ -17,12 +17,14 @@ function choiceHeader() {
 		tHeader.removeAttribute("hidden");
 		menuNav.setAttribute("hidden", "hidden");
 		menuBtn.setAttribute("hidden", "hidden");
+		popup.setAttribute("hidden", "hidden");
 		toggleMenu();
 	} else {
 		showMenu = true;
 		tHeader.setAttribute("hidden", "hidden");
 		menuNav.removeAttribute("hidden");
 		menuBtn.removeAttribute("hidden");
+		popup.removeAttribute("hidden");
 		toggleMenu();
 	}
 }
@@ -31,17 +33,37 @@ menuBtn.addEventListener('click', toggleMenu);
 
 
 function toggleMenu() {
+	let tumbler = menuBtn.getAttribute("aria-expanded");
+	switch (tumbler) {
+		case "true":
+			menuNav.removeAttribute("hidden");
+			break;
+		case "false":
+			menuNav.setAttribute("hidden", "hidden");
+			break;
+		case null:
+			menuNav.setAttribute("hidden", "hidden");
+			break;
+		default:
+			menuNav.setAttribute("hidden", "hidden");
+			break;
+	}
+
 	if (!showMenu) {
 		hamburger.classList.add('open');
 		nav.classList.add('open');
 		menuNav.classList.add('open');
 		menuBtn.classList.toggle("active");
+		menuBtn.setAttribute("aria-expanded", "false");
 		navItems.forEach(item => item.classList.add('open'));
+		popup.classList.toggle("open");
+		popup.appendChild(menuNav);
 		showMenu = true;
 	} else {
 		hamburger.classList.remove('open');
 		nav.classList.remove('open');
 		menuNav.classList.remove('open');
+		menuBtn.setAttribute("aria-expanded", "true");
 		navItems.forEach(item => item.classList.remove('open'));
 		showMenu = false;
 	}
