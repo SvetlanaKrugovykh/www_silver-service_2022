8383ab1e5bad326811ee653ef0279a58fc0b2fcc
SvetlanaKrugovykh <Svetlana.Krugovykh@gmail.com>
2022-11-17 12:47:37 +0200

    adding 3 domains:			"www.silver-service.com.ua", 			"www.xn--80aa2aboqt7n.xn--j1amh", 			"www.xn--80acd5bg5aou4k.net"

--- js/main.js
+++ js/main.js
@@ -1,28 +1,8 @@
 //#region pageContentNames
 const pageContent = [
 	{
-		"page": "SILVER_SERVICE",
-		"basePage": "index.html",
-		"forHide": ["div[field='tn_text_1578322192366']",
-			"[id ^= 'rec149442898']",
-			"[id ^= 'rec149504840']",
-			"[id ^= 'rec149521426']",
-			".owl-stage-outer"
-		],
-		"keysForReDefine": [".t-feed__post-preloader__textwrapper"],
-		"valuesForReDefine": ["font-size: 14px;"],
-		"toAdd": [
-			{
-				"id": "imageIndex_1",
-				"where": "[id ^= 'rec153154582']",
-				"img": "images/backgrIndex.png",
-				"href": "index.html"
-			}
-		]
-	},
-	{
 		"page": "INTERNET",
-		"basePage": "page8498574.html",
+		"basePage": ["page8498574.html"],
 		"forHide": [
 			".t396",
 			"#rec150333633",
@@ -42,7 +22,7 @@ const pageContent = [
 	},
 	{
 		"page": "TV",
-		"basePage": "page8629852.html",
+		"basePage": ["page8629852.html"],
 		"forHide": [
 			".t396",
 			"#rec153306226"
@@ -57,7 +37,40 @@ const pageContent = [
 				"href": "page8629852.html"
 			}
 		]
-	}
+	},
+	{
+		"page": "НОВИНИ",
+		"basePage": ["page8629856.html'"],
+		"forHide": [],
+		"keysForReDefine": [],
+		"valuesForReDefine": [],
+		"toAdd": []
+	},
+	{
+		"page": "SILVER_SERVICE",
+		"basePage": [
+			"index.html",
+			"www.silver-service.com.ua",
+			"www.xn--80aa2aboqt7n.xn--j1amh",
+			"www.xn--80acd5bg5aou4k.net"
+		],
+		"forHide": ["div[field='tn_text_1578322192366']",
+			"[id ^= 'rec149442898']",
+			"[id ^= 'rec149504840']",
+			"[id ^= 'rec149521426']",
+			".owl-stage-outer"
+		],
+		"keysForReDefine": [".t-feed__post-preloader__textwrapper"],
+		"valuesForReDefine": ["font-size: 14px;"],
+		"toAdd": [
+			{
+				"id": "imageIndex_1",
+				"where": "[id ^= 'rec153154582']",
+				"img": "images/backgrIndex.png",
+				"href": "index.html"
+			}
+		]
+	},
 ];
 //#endregion
 
@@ -143,16 +156,18 @@ function toggleMenu() {
 //#region hidingElements
 function hideDivs() {
 	for (const page of pageContent) {
-		if (document.baseURI.includes(page.basePage)) {
-			for (let element of page.forHide) {
-				try {
-					let elements = document.querySelectorAll(element);
-					for (let elem of elements) {
-						if (elem != null) elem.setAttribute("style", "display: none");
-					}
-				} catch (error) { console.log(error); }
+		for (const basePage of page.basePage) {
+			if (document.baseURI.includes(basePage)) {
+				for (let element of page.forHide) {
+					try {
+						let elements = document.querySelectorAll(element);
+						for (let elem of elements) {
+							if (elem != null) elem.setAttribute("style", "display: none");
+						}
+					} catch (error) { console.log(error); }
+				}
+				return;
 			}
-			break;
 		}
 	}
 }
@@ -162,33 +177,35 @@ function hideDivs() {
 function reDefineDivs() {
 
 	for (const page of pageContent) {
-		if (document.baseURI.includes(page.basePage)) {
-			let i = 0;
-			for (let element of page.keysForReDefine) {
-				i++;
-				try {
-					let elements = document.querySelectorAll(element);
-					for (let elem of elements) {
-						if (elem != null) elem.setAttribute("style", page.valuesForReDefine[i - 1]);
-					}
-				} catch (error) { console.log(error); }
-			}
-			for (let element of page.toAdd) {
-				let where = document.querySelector(element.where);
-				if (where == null) continue;
-				let img = document.createElement("img");
-				let a = document.createElement("a");
-				a.id = element.id;
-				a.href = element.href;
-				a.style.display = "flex";
-				img.style.width = "100%";
-				img.src = element.img;
-				if (document.querySelector(`[id ^= '${element.id}']`) == null) {
-					where.insertAdjacentElement("beforebegin", a);
-					a.appendChild(img);
+		for (const basePage of page.basePage) {
+			if (document.baseURI.includes(basePage)) {
+				let i = 0;
+				for (let element of page.keysForReDefine) {
+					i++;
+					try {
+						let elements = document.querySelectorAll(element);
+						for (let elem of elements) {
+							if (elem != null) elem.setAttribute("style", page.valuesForReDefine[i - 1]);
+						}
+					} catch (error) { console.log(error); }
 				}
+				for (let element of page.toAdd) {
+					let where = document.querySelector(element.where);
+					if (where == null) continue;
+					let img = document.createElement("img");
+					let a = document.createElement("a");
+					a.id = element.id;
+					a.href = element.href;
+					a.style.display = "flex";
+					img.style.width = "100%";
+					img.src = element.img;
+					if (document.querySelector(`[id ^= '${element.id}']`) == null) {
+						where.insertAdjacentElement("beforebegin", a);
+						a.appendChild(img);
+					}
+				}
+				return;
 			}
-			break;
 		}
 	}
 }
