13ed413cf56c850838e6c48fada2c13af13cce6b
SvetlanaKrugovykh <Svetlana.Krugovykh@gmail.com>
2023-07-18 17:33:37 +0300

    Added adaptivity for redirect page

--- js/main.js
+++ js/main.js
@@ -1,77 +1,95 @@
 //#region pageContentNames
 const pageContent = [
-	{
-		"page": "INTERNET",
-		"basePage": ["page8498574.html"],
-		"forHide": [
-			".t396",
-			"#rec150333633",
-			"#rec150338650",
-			"#rec150333603"
-		],
-		"keysForReDefine": [],
-		"valuesForReDefine": [],
-		"toAdd": [
-			{
-				"id": "imageIndex_2",
-				"where": "#t-footer",
-				"img": "images/backgrIndex.png",
-				"href": "page8498574.html"
-			}
-		]
-	},
-	{
-		"page": "TV",
-		"basePage": ["page8629852.html"],
-		"forHide": [
-			".t396",
-			"#rec153306226"
-		],
-		"keysForReDefine": [],
-		"valuesForReDefine": [],
-		"toAdd": [
-			{
-				"id": "imageIndex_3",
-				"where": "#t-footer",
-				"img": "images/backgrTV.png",
-				"href": "page8629852.html"
-			}
-		]
-	},
-	{
-		"page": "НОВИНИ",
-		"basePage": ["page8629856.html'"],
-		"forHide": [],
-		"keysForReDefine": [],
-		"valuesForReDefine": [],
-		"toAdd": []
-	},
-	{
-		"page": "SILVER_SERVICE",
-		"basePage": [
-			"index.html",
-			"www.silver-service.com.ua",
-			"www.xn--80aa2aboqt7n.xn--j1amh",
-			"www.xn--80acd5bg5aou4k.net",
-			"www_silver-service_2022"
-		],
-		"forHide": ["div[field='tn_text_1578322192366']",
-			"[id ^= 'rec149442898']",
-			"[id ^= 'rec149504840']",
-			"[id ^= 'rec149521426']",
-			".owl-stage-outer"
-		],
-		"keysForReDefine": [".t-feed__post-preloader__textwrapper"],
-		"valuesForReDefine": ["font-size: 14px;"],
-		"toAdd": [
-			{
-				"id": "imageIndex_1",
-				"where": "[id ^= 'rec153154582']",
-				"img": "images/backgrIndex.png",
-				"href": "index.html"
-			}
-		]
-	},
+  {
+    "page": "INTERNET",
+    "basePage": ["page8498574.html"],
+    "forHide": [
+      ".t396",
+      "#rec150333633",
+      "#rec150338650",
+      "#rec150333603"
+    ],
+    "keysForReDefine": [],
+    "valuesForReDefine": [],
+    "toAdd": [
+      {
+        "id": "imageIndex_2",
+        "where": "#t-footer",
+        "img": "images/backgrIndex.png",
+        "href": "page8498574.html"
+      }
+    ]
+  },
+  {
+    "page": "TV",
+    "basePage": ["page8629852.html"],
+    "forHide": [
+      ".t396",
+      "#rec153306226"
+    ],
+    "keysForReDefine": [],
+    "valuesForReDefine": [],
+    "toAdd": [
+      {
+        "id": "imageIndex_3",
+        "where": "#t-footer",
+        "img": "images/backgrTV.png",
+        "href": "page8629852.html"
+      }
+    ]
+  },
+  {
+    "page": "Redirect",
+    "basePage": ["redirect.html"],
+    "forHide": [
+      ".t396",
+      "#rec153306226"
+    ],
+    "keysForReDefine": [],
+    "valuesForReDefine": [],
+    "toAdd": [
+      {
+        "id": "imageIndex_4",
+        "where": "#t-footer",
+        "img": "images/backgrRdrct.png",
+        "href": "redirect.html"
+      }
+    ]
+  },
+  {
+    "page": "НОВИНИ",
+    "basePage": ["page8629856.html'"],
+    "forHide": [],
+    "keysForReDefine": [],
+    "valuesForReDefine": [],
+    "toAdd": []
+  },
+  {
+    "page": "SILVER_SERVICE",
+    "basePage": [
+      "index.html",
+      "www.silver-service.com.ua",
+      "www.xn--80aa2aboqt7n.xn--j1amh",
+      "www.xn--80acd5bg5aou4k.net",
+      "www_silver-service_2022"
+    ],
+    "forHide": ["div[field='tn_text_1578322192366']",
+      "[id ^= 'rec149442898']",
+      "[id ^= 'rec149504840']",
+      "[id ^= 'rec149521426']",
+      ".owl-stage-outer"
+    ],
+    "keysForReDefine": [".t-feed__post-preloader__textwrapper"],
+    "valuesForReDefine": ["font-size: 14px;"],
+    "toAdd": [
+      {
+        "id": "imageIndex_1",
+        "where": "[id ^= 'rec153154582']",
+        "img": "images/backgrIndex.png",
+        "href": "index.html"
+      }
+    ]
+  },
 ];
 //#endregion
 
@@ -90,22 +108,22 @@ choiceHeader();
 window.addEventListener('resize', choiceHeader);
 
 function choiceHeader() {
-	if (window.innerWidth > 768) {
-		showMenu = false;
-		tHeader.removeAttribute("hidden");
-		menuNav.setAttribute("hidden", "hidden");
-		menuBtn.setAttribute("hidden", "hidden");
-		popup.setAttribute("hidden", "hidden");
-		toggleMenu();
-	} else {
-		showMenu = true;
-		tHeader.setAttribute("hidden", "hidden");
-		menuNav.removeAttribute("hidden");
-		menuBtn.removeAttribute("hidden");
-		popup.removeAttribute("hidden");
-		toggleMenu();
-		launchPageRebuilding();
-	}
+  if (window.innerWidth > 768) {
+    showMenu = false;
+    tHeader.removeAttribute("hidden");
+    menuNav.setAttribute("hidden", "hidden");
+    menuBtn.setAttribute("hidden", "hidden");
+    popup.setAttribute("hidden", "hidden");
+    toggleMenu();
+  } else {
+    showMenu = true;
+    tHeader.setAttribute("hidden", "hidden");
+    menuNav.removeAttribute("hidden");
+    menuBtn.removeAttribute("hidden");
+    popup.removeAttribute("hidden");
+    toggleMenu();
+    launchPageRebuilding();
+  }
 }
 
 //#region toggleMenu
@@ -113,40 +131,40 @@ menuBtn.addEventListener('click', toggleMenu);
 
 
 function toggleMenu() {
-	let tumbler = menuBtn.getAttribute("aria-expanded");
-	switch (tumbler) {
-		case "true":
-			menuNav.removeAttribute("hidden");
-			break;
-		case "false":
-			menuNav.setAttribute("hidden", "hidden");
-			break;
-		case null:
-			menuNav.setAttribute("hidden", "hidden");
-			break;
-		default:
-			menuNav.setAttribute("hidden", "hidden");
-			break;
-	}
+  let tumbler = menuBtn.getAttribute("aria-expanded");
+  switch (tumbler) {
+    case "true":
+      menuNav.removeAttribute("hidden");
+      break;
+    case "false":
+      menuNav.setAttribute("hidden", "hidden");
+      break;
+    case null:
+      menuNav.setAttribute("hidden", "hidden");
+      break;
+    default:
+      menuNav.setAttribute("hidden", "hidden");
+      break;
+  }
 
-	if (!showMenu) {
-		hamburger.classList.add('open');
-		nav.classList.add('open');
-		menuNav.classList.add('open');
-		menuBtn.classList.toggle("active");
-		menuBtn.setAttribute("aria-expanded", "false");
-		navItems.forEach(item => item.classList.add('open'));
-		popup.classList.toggle("open");
-		popup.appendChild(menuNav);
-		showMenu = true;
-	} else {
-		hamburger.classList.remove('open');
-		nav.classList.remove('open');
-		menuNav.classList.remove('open');
-		menuBtn.setAttribute("aria-expanded", "true");
-		navItems.forEach(item => item.classList.remove('open'));
-		showMenu = false;
-	}
+  if (!showMenu) {
+    hamburger.classList.add('open');
+    nav.classList.add('open');
+    menuNav.classList.add('open');
+    menuBtn.classList.toggle("active");
+    menuBtn.setAttribute("aria-expanded", "false");
+    navItems.forEach(item => item.classList.add('open'));
+    popup.classList.toggle("open");
+    popup.appendChild(menuNav);
+    showMenu = true;
+  } else {
+    hamburger.classList.remove('open');
+    nav.classList.remove('open');
+    menuNav.classList.remove('open');
+    menuBtn.setAttribute("aria-expanded", "true");
+    navItems.forEach(item => item.classList.remove('open'));
+    showMenu = false;
+  }
 
 }
 //#endregion 
@@ -156,67 +174,67 @@ function toggleMenu() {
 
 //#region hidingElements
 function hideDivs() {
-	for (const page of pageContent) {
-		for (const basePage of page.basePage) {
-			if (document.baseURI.includes(basePage)) {
-				for (let element of page.forHide) {
-					try {
-						let elements = document.querySelectorAll(element);
-						for (let elem of elements) {
-							if (elem != null) elem.setAttribute("style", "display: none");
-						}
-					} catch (error) { console.log(error); }
-				}
-				return;
-			}
-		}
-	}
+  for (const page of pageContent) {
+    for (const basePage of page.basePage) {
+      if (document.baseURI.includes(basePage)) {
+        for (let element of page.forHide) {
+          try {
+            let elements = document.querySelectorAll(element);
+            for (let elem of elements) {
+              if (elem != null) elem.setAttribute("style", "display: none");
+            }
+          } catch (error) { console.log(error); }
+        }
+        return;
+      }
+    }
+  }
 }
 //#endregion
 
 //#region reDefineElements
 function reDefineDivs() {
 
-	for (const page of pageContent) {
-		for (const basePage of page.basePage) {
-			if (document.baseURI.includes(basePage)) {
-				let i = 0;
-				for (let element of page.keysForReDefine) {
-					i++;
-					try {
-						let elements = document.querySelectorAll(element);
-						for (let elem of elements) {
-							if (elem != null) elem.setAttribute("style", page.valuesForReDefine[i - 1]);
-						}
-					} catch (error) { console.log(error); }
-				}
-				for (let element of page.toAdd) {
-					let where = document.querySelector(element.where);
-					if (where == null) continue;
-					let img = document.createElement("img");
-					let a = document.createElement("a");
-					a.id = element.id;
-					a.href = element.href;
-					a.style.display = "flex";
-					img.style.width = "100%";
-					img.src = element.img;
-					if (document.querySelector(`[id ^= '${element.id}']`) == null) {
-						where.insertAdjacentElement("beforebegin", a);
-						a.appendChild(img);
-					}
-				}
-				return;
-			}
-		}
-	}
+  for (const page of pageContent) {
+    for (const basePage of page.basePage) {
+      if (document.baseURI.includes(basePage)) {
+        let i = 0;
+        for (let element of page.keysForReDefine) {
+          i++;
+          try {
+            let elements = document.querySelectorAll(element);
+            for (let elem of elements) {
+              if (elem != null) elem.setAttribute("style", page.valuesForReDefine[i - 1]);
+            }
+          } catch (error) { console.log(error); }
+        }
+        for (let element of page.toAdd) {
+          let where = document.querySelector(element.where);
+          if (where == null) continue;
+          let img = document.createElement("img");
+          let a = document.createElement("a");
+          a.id = element.id;
+          a.href = element.href;
+          a.style.display = "flex";
+          img.style.width = "100%";
+          img.src = element.img;
+          if (document.querySelector(`[id ^= '${element.id}']`) == null) {
+            where.insertAdjacentElement("beforebegin", a);
+            a.appendChild(img);
+          }
+        }
+        return;
+      }
+    }
+  }
 }
 //#endregion
 
 
 //#region calls
 function launchPageRebuilding() {
-	hideDivs();
-	reDefineDivs();
+  hideDivs();
+  reDefineDivs();
 }
 //#endregion
 
